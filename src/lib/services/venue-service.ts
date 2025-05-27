b61f5716e496e041dd0d28ac2fe510a488ec3b98
modrisco89 <geralt.odriscoll@gmail.com>
2025-05-27 20:17:14 +0100

    netlify updates 3

--- src/lib/services/venue-service.ts
+++ src/lib/services/venue-service.ts
@@ -4,7 +4,7 @@ import { currentInfos, currentVenues, loggedInUser } from "$lib/runes.svelte";
 
 
 export const venueService = {
-  baseUrl: "https://concise-foam-newsprint.glitch.me",
+  baseUrl: "http://2bdcefc79e52:3000",
   
   async signup(user: User): Promise<boolean> {
     try {
